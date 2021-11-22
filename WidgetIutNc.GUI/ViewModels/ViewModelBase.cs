3d41ea72733aaabd6f94cb18a15eddd5dbf18662
Alexis C. GRIDEL <contact@agdl.dev>
2021-11-22 18:29:33 +0100

    feat: init view model base for main design application pattern

---
+++ WidgetIutNc.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WidgetIutNc.GUI.ViewModels
+{
+    public class ViewModelBase
+    : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
+
+
+
