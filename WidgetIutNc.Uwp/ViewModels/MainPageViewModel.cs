9bef6a96c0b1bd0dcb363de1968b97025dc9adad
Alexis C. GRIDEL <contact@agdl.dev>
2021-11-24 22:15:56 +0100

    feat: assume viewmodels integrity

---
+++ WidgetIutNc.Uwp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Ical.Net;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Reactive;
+using WidgetIutNc.Api;
+
+namespace WidgetIutNc.Uwp.ViewModels;
+
+public class MainPageViewModel
+    : ViewModelBase
+{
+    private readonly IUpdatedCalendarFileDownloaderService _calendarFileDownloaderService;
+
+    public MainPageViewModel(IUpdatedCalendarFileDownloaderService calendarFileDownloaderService)
+    {
+        _calendarFileDownloaderService = calendarFileDownloaderService;
+    }
+
+    [Reactive]
+    public string Description { get; set; }
+
+    public ReactiveCommand<Unit, Calendar> RefreshDataAsync { get; }
+
+    public MainPageViewModel()
+    {
+        RefreshDataAsync = ReactiveCommand.CreateFromTask(async () => await _calendarFileDownloaderService.GetUpdatedCalendarFileAsync().ConfigureAwait(true));
+        RefreshDataAsync.BindTo(this, x => x.Description);
+    }
+}
+
--- WidgetIutNc.Uwp/ViewModels/ViewModelBase.cs
+++ WidgetIutNc.Uwp/ViewModels/ViewModelBase.cs
@@ -1,16 +1,10 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using ReactiveUI;
 
 namespace WidgetIutNc.Uwp.ViewModels
 {
     public class ViewModelBase
-        : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    }
+        : ReactiveObject
+    { }
 }
 
 
