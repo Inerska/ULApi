ad2f34d27f36fa80858f2c41739882752754f718
Alexis C. Gridel <contact@agdl.dev>
2021-11-25 21:30:58 +0100

    feat: parsed concrete calendar entity

---
+++ WidgetIutNc.Api/Entities/ParsedConcreteCalendar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WidgetIutNc.Api.Entities
+{
+    public record class ParsedConcreteCalendar(
+        DateTime StartDate,
+        DateTime EndDate,
+        string Summary,
+        string Location,
+        string Description);
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class IsExternalInit { }
+}
