6f2d618113612d65179d5506a1f70852d0831acd
Alexis C. Gridel <contact@agdl.dev>
2021-11-27 11:21:03 +0100

    feat: add controls

--- WidgetIutNc.Uwp/Controls/ScheduleCell.xaml.cs
+++ WidgetIutNc.Uwp/Controls/ScheduleCell.xaml.cs
@@ -1,14 +1,27 @@
-﻿using WidgetIutNc.ViewModels.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-namespace WidgetIutNc.Uwp.Controls;
-public sealed partial class ScheduleCell
-    : UserControl
+// Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WidgetIutNc.Uwp.Controls
 {
-    public ScheduleCell(ScheduleCellViewModel scheduleCellViewModel)
+    public sealed partial class ScheduleCell : UserControl
     {
-        InitializeComponent();
-
-        this.DataContext = scheduleCellViewModel;
+        public ScheduleCell()
+        {
+            this.InitializeComponent();
+        }
     }
 }
