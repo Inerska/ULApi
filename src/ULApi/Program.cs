87604d4f608ce7647a328d401a49f61215e8338e
Alexis C. Gridel <alexis.gridel@pm.me>
2021-12-09 22:53:41 +0100

    feat: add swagger in dev mode

--- src/ULApi/Program.cs
+++ src/ULApi/Program.cs
@@ -5,8 +5,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = String.Empty;
+    });
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/twit", () => "Hey");
