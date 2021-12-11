e56843c3597bdf544f33362bc1a447657e058f89
Alexis C. Gridel <contact@agdl.dev>
2021-12-11 17:03:00 +0100

    feat: add file header template to whole solution

--- src/ULApi/Controllers/WeatherForecastController.cs
+++ src/ULApi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace ULApi.Controllers
