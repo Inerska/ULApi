32393119a4eb95bfd9f1b244cb328baadae5be82
Alexis C. Gridel <contact@agdl.dev>
2021-12-11 16:51:41 +0100

    refractor: main solutionand projects

---
+++ src/ULApi/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ULApi.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class TestController
+    : Controller
+{
+    [HttpGet]
+    public string[] GetSampleDatasString()
+    {
+        return new string[] { "hey", "sir" };
+    }
+}
--- src/ULApi/Program.cs
+++ src/ULApi/Program.cs
@@ -1,29 +1,25 @@
-// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
-// Licensed under the GNU General Public License v3.0.
-// See the LICENSE file in the project root for more information.
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options => {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = String.Empty;
-    });
-    app.UseDeveloperExceptionPage();
+    app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/twit", () => "Hey");
+app.UseHttpsRedirection();
 
-app.UseHttpMethodOverride();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
