bc5d7c2f07bdf8e8c894bfe54d02df09ad6f48fa
Alexis C. Gridel <contact@agdl.dev>
2021-12-12 00:40:35 +0100

    feat: business layer and first fetcher functions

---
+++ src/ULApi.BusinessLayer/GraphBusinessFetcherService.cs
@@ -0,0 +1,19 @@
+﻿namespace ULApi.Services;
+
+/// <summary>
+/// Concrete implementation of Graph strategy business layer fetcher service.
+/// </summary>
+/// <typeparam name="TItem"></typeparam>
+public class GraphBusinessFetcherService<TItem>
+    : IBusinessFetcherService<TItem>
+{
+    public TItem? Fetch()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TItem?> FetchAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ULApi.BusinessLayer/IBusinessFetcherService.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace ULApi.Services;
+
+/// <summary>
+/// Interface of server fetcher business layer.
+/// </summary>
+/// <typeparam name="TItem">Fetcher item to retrieve.</typeparam>
+public interface IBusinessFetcherService<TItem>
+{
+    /// <summary>
+    /// Synchronous fetching data method.
+    /// </summary>
+    /// <returns>Nullable fetching entity referenced as model.</returns>
+    TItem? Fetch();
+
+    /// <summary>
+    /// Asynchronous fetching data method.
+    /// </summary>
+    /// <returns>Nullable task of a fetching entity referenced as model.</returns>
+    Task<TItem?> FetchAsync();
+}
+
