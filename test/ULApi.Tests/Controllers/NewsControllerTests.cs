bfd301912fed8c3aa3ddf41c7ed3469ccee64be1
Alexis C. Gridel <alexis.gridel@pm.me>
2021-12-13 12:48:37 +0100

    feat: add unit test to news controller, test initialize in ctor format

--- test/ULApi.Tests/Controllers/NewsControllerTests.cs
+++ test/ULApi.Tests/Controllers/NewsControllerTests.cs
@@ -1,8 +1,29 @@
-﻿using Xunit;
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using ULApi.Controllers;
+using Xunit;
 
 namespace ULApi.Test.Controllers;
 public class NewsControllerTests
 {
-    [Fact]
+    private readonly Mock<IConfiguration> _configuration;
+    private NewsController _controller;
 
+    /// <summary>
+    /// Test initialization, inject mock IConfiguration to News controller.
+    /// </summary>
+    public NewsControllerTests()
+    {
+        _configuration = new Mock<IConfiguration>();
+        _controller = new NewsController(_configuration.Object);
+    }
+
+    [Fact]
+    public void GetMethod_WithCount_Should_Return_News()
+    {
+        var count = 1;
+        var response = _controller.Get(count);
+
+        Assert.NotNull(response);
+    }
 }
