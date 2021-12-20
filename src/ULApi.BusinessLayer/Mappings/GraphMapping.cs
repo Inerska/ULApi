256c41081f8066a9dbea62229ba88e9d8686e330
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-12-20 22:25:18 +0100

    refactor: add builder inside GraphMapping and Node

--- src/ULApi.BusinessLayer/Mappings/GraphMapping.cs
+++ src/ULApi.BusinessLayer/Mappings/GraphMapping.cs
@@ -2,120 +2,48 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections;
+
 namespace ULApi.BusinessLayer.Mappings;
 
 /// <summary>
-/// Represents a Graph object relational mapping.
+///     Represents a Graph object relational mapping.
 /// </summary>
 public class GraphMapping
+    : IEnumerable
 {
-    public string Value { get; set; }
-    public GraphMapping Parent { get; set; }
-    public GraphMapping[] Children { get; set; }
+    /// <summary>
+    ///     Collection of nodes that are part of the graph.
+    /// </summary>
+    private readonly IList<Node> _nodes = new List<Node>();
 
     /// <summary>
-    /// Initializes a new instance of <see cref="GraphMapping"/> class.
+    ///     Get the count of the collection of nodes.
     /// </summary>
-    /// <param name="value">Value to set.</param>
-    /// <param name="isRoot">Add query keyword before.</param>
-    /*public GraphMapping(
-        string value,
-        bool isRoot = false)
+    public int Count => _nodes.Count;
+
+    IEnumerator IEnumerable.GetEnumerator()
     {
-        _parent = null;
-        _value = isRoot
-            ? $"query {value}"
-            : value;
-        _children = Array.Empty<GraphMapping>();
-    }*/
+        return _nodes.GetEnumerator();
+    }
+
 
     /// <summary>
-    /// Add a <see cref="GraphMapping"/> children to the current instance.
+    ///     Add overload for <see cref="Node" />, accepting a value.
     /// </summary>
-    /// <param name="children"><see cref="GraphMapping"/> instance to add.</param>
-    /// <returns>The <see cref="GraphMapping"/> children.</returns>
-    public GraphMapping AddChildren(GraphMapping children)
+    /// <param name="value"></param>
+    public void Add(string value)
     {
-        var newChildren = new GraphMapping[_children.Length + 1];
-        for (var i = 0; i < _children.Length; i++)
-        {
-            newChildren[i] = _children[i];
-        }
-
-        newChildren[_children.Length] = children;
-        _children = newChildren;
-        children._parent = this;
-
-        return children;
+        _nodes.Add(new Node(value));
     }
 
     /// <summary>
-    /// Add a <see cref="GraphMapping"/> children to the current instance.
+    ///     Add overload for <see cref="Node" />, accepting a value and Children.
     /// </summary>
-    /// <param name="value">Value of the children.</param>
-    /// <returns>The <see cref="GraphMapping"/> children.</returns>
-    public GraphMapping AddChildren(string value)
+    /// <param name="hey"></param>
+    /// <param name="hes"></param>
+    public void Add(string value, GraphMapping children)
     {
-        var newChildren = new GraphMapping[_children.Length + 1];
-        for (var i = 0; i < _children.Length; i++)
-        {
-            newChildren[i] = _children[i];
-        }
-
-        var children = new GraphMapping(value);
-
-        newChildren[_children.Length] = children;
-        _children = newChildren;
-        children._parent = this;
-
-        return children;
-    }
-
-    /// <summary>
-    /// The number of parents of the current instance.
-    /// </summary>
-    /// <returns></returns>
-    public int ParentSize()
-        => _parent is not null
-            ? _parent.ParentSize() + 1
-            : 0;
-
-    /// <summary>
-    /// Stringify the <see cref="GraphMapping"/> instance.
-    /// </summary>
-    /// <returns>A string that represents the current instance.</returns>
-    public string RenderQuery()
-    {
-        var s = "";
-        if (_children.Length != 0)
-        {
-            s += _value + "{\r\n";
-        }
-        else
-        {
-            s += _value;
-        }
-        foreach (var t in _children)
-        {
-            for (var j = 0; j < ParentSize(); j++)
-            {
-                s += "    ";
-            }
-            s += t.RenderQuery();
-        }
-
-        if (_children.Length == 0)
-        {
-            return s + "\r\n";
-        }
-
-        {
-            for (var j = 0; j < ParentSize(); ++j)
-            {
-                s += "    ";
-            }
-            return s + "}\r\n";
-        }
-
+        _nodes.Add(new Node(value, children));
     }
 }
